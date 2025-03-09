86136708f93fd594e3be2ace0defc228169fd925
AdoDeveloper <105835711+AdoDeveloper@users.noreply.github.com>
2025-03-09 09:31:14 -0600

    ajustes en la semilla

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,27 +4,75 @@ const bcrypt = require("bcryptjs");
 const prisma = new PrismaClient();
 
 async function main() {
+  // Crear o actualizar los roles:
+  await prisma.role.upsert({
+    where: { name: "Administrador" },
+    update: {},
+    create: { name: "Administrador" },
+  });
+  await prisma.role.upsert({
+    where: { name: "Asistente Operativo" },
+    update: {},
+    create: { name: "Asistente Operativo" },
+  });
+  console.log("Roles creados correctamente ✅");
+
+  // Array de usuarios con sus datos, asignando a Almapac el rol admin (id: 1)
+  // y a los demás el rol user (id: 2).
   const users = [
-    { id: 1, username: "Almapac", email: "almapac@sistemademoras.com", password: "Almapac2025" },
-    { id: 2264, username: "Adolfo Cortez", email: "adolfo@sistemademoras.com", password: "Almapac2025" },
-    { id: 2265, username: "Marcela Cuevas", email: "marcela@sistemademoras.com", password: "Almapac2025" },
-    { id: 2266, username: "Gabriela", email: "gabriela@sistemademoras.com", password: "Almapac2025" },
+    {
+      id: 1,
+      username: "Almapac",
+      email: "almapac@sistemademoras.com",
+      password: "Almapac2025",
+      nombreCompleto: "Almapac",
+      codigo: "ALMAPAC",
+      roleId: 1,
+    },
+    {
+      id: 2,
+      username: "Adolfo",
+      email: "adolfo@sistemademoras.com",
+      password: "Almapac2025",
+      nombreCompleto: "Adolfo Cortez",
+      codigo: "2264",
+      roleId: 2,
+    },
+    {
+      id: 3,
+      username: "Marcela",
+      email: "marcela@sistemademoras.com",
+      password: "Almapac2025",
+      nombreCompleto: "Marcela Cuevas",
+      codigo: "2265",
+      roleId: 2,
+    },
+    {
+      id: 4,
+      username: "Gabriela",
+      email: "gabriela@sistemademoras.com",
+      password: "Almapac2025",
+      nombreCompleto: "Gabriela Pimentel",
+      codigo: "2266",
+      roleId: 2,
+    },
   ];
 
   for (const user of users) {
     const hashedPassword = await bcrypt.hash(user.password, 10);
-
     await prisma.user.upsert({
       where: { email: user.email },
       update: {},
       create: {
         username: user.username,
+        nombreCompleto: user.nombreCompleto,
+        codigo: user.codigo,
         email: user.email,
         password: hashedPassword,
+        roleId: user.roleId,
       },
     });
   }
-
   console.log("Usuarios creados correctamente ✅");
 }
 
@@ -35,4 +83,4 @@ main()
   })
   .finally(async () => {
     await prisma.$disconnect();
-  });
+});
